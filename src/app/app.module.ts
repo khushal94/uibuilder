ba4aeaf33d50dd773ca62510f2e49d718152d421
keldon <khushal.yadav@fxvisuals.in>
2023-06-06 15:19:18 +0530

    UI builder test commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormServiceService } from './form-service.service';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { FormServiceService } from '../form-service.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public formJsonData: any;
+  public currentPageId: string = '1';
+  public showMsg = '';
 
-  constructor() {}
+  constructor(public formService: FormServiceService) {
+
+    this.formJsonData = formService.formJson.pages;
+
+  }
+
+  btnEvents(elementData: any) {
+
+    if (elementData.text === 'Save') {
+      this.showMsg = 'Thanks for submitting your details!';
+      console.log(this.formJsonData, '<-----final submitted values, inside every input object accessible via .value');
+    }
+
+    if (elementData.navigate) {
+      this.currentPageId = elementData.navigate
+    }
+
+  }
+
 
 }
